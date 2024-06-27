c33716f3fe5d22a532efba3e0aa5656691b65053
Marco Roccon <marcoroccon@MacBook-Pro-di-Marco-2.local>
2024-06-27 16:23:33 +0200

    Eliminati import superflui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Button, Text } from "@chakra-ui/react";
 import Header from "./components/Header/Header";
 import Feed from "./components/Feed/Feed";
 import Footer from "./components/Footer/Footer";
