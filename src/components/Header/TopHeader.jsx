1d705611658b0523d1b42b787b06ff090f504ac3
Marco Roccon <marcoroccon@MBP-di-Marco-2.homenet.telecomitalia.it>
2024-07-05 01:33:06 +0200

    Refresh/back to home cliccando sul logo

--- src/components/Header/TopHeader.jsx
+++ src/components/Header/TopHeader.jsx
@@ -3,12 +3,16 @@ import React from "react";
 import { FaUser } from "react-icons/fa";
 import logo from "../../img/nyt-logo.png";
 import LeftSideDrawer from "./LeftSideDrawer";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const TopHeader = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
   return (
     <>
       <LeftSideDrawer />
-      <Image src={logo} h={{base: '42px', md:'48px', lg:'76px'}}></Image>
+      <Image src={logo} h={{base: '42px', md:'48px', lg:'76px'}} cursor={'pointer'} 
+      onClick={location.pathname !== "/" ? () => navigate("/") : () => window.location.reload()}></Image>
       <Button variant={'link'} textDecoration={'none'}>
         <FaUser color="black" />
       </Button>
