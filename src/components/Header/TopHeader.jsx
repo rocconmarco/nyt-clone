27b46d7fd31a205e5a389c12fe9aa92047e6bc74
Marco Roccon <marcoroccon@MBP-di-Marco-2.homenet.telecomitalia.it>
2024-07-05 12:48:09 +0200

    Implementato routing icona utente in alto a destra

--- src/components/Header/TopHeader.jsx
+++ src/components/Header/TopHeader.jsx
@@ -4,16 +4,18 @@ import { FaUser } from "react-icons/fa";
 import logo from "../../img/nyt-logo.png";
 import LeftSideDrawer from "./LeftSideDrawer";
 import { useLocation, useNavigate } from "react-router-dom";
+import useAuthStore from "../../store/authStore";
 
 const TopHeader = () => {
   const navigate = useNavigate()
   const location = useLocation()
+  const authUser = useAuthStore((state) => state.user);
   return (
     <>
       <LeftSideDrawer />
       <Image src={logo} h={{base: '42px', md:'48px', lg:'76px'}} cursor={'pointer'} 
       onClick={location.pathname !== "/" ? () => navigate("/") : () => window.location.reload()}></Image>
-      <Button variant={'link'} textDecoration={'none'}>
+      <Button variant={'link'} textDecoration={'none'} onClick={authUser ? () => navigate("/members") : () => navigate("/auth") }>
         <FaUser color="black" />
       </Button>
     </>
