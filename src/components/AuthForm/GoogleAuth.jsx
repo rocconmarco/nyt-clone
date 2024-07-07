725a02b4ec4864896091989df13938a56aba9ee2
Marco Roccon <marcoroccon@MBP-di-Marco-2.homenet.telecomitalia.it>
2024-07-07 12:37:19 +0200

    Implementato login Google

--- src/components/AuthForm/GoogleAuth.jsx
+++ src/components/AuthForm/GoogleAuth.jsx
@@ -5,21 +5,28 @@ import { FcGoogle } from "react-icons/fc";
 import { auth, firestore } from "../../firebase/firebase";
 import useShowToast from "../../hooks/useShowToast";
 import useAuthStore from "../../store/authStore";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
-const GoogleAuth = ({prefix}) => {
-  const [signInWithGoogle, error] = useSignInWithGoogle(auth)
-  const showToast = useShowToast()
-  const loginUser = useAuthStore((state) => state.login)
+const GoogleAuth = ({ prefix }) => {
+  const [signInWithGoogle, error] = useSignInWithGoogle(auth);
+  const showToast = useShowToast();
+  const loginUser = useAuthStore((state) => state.login);
 
   const handleGoogleAuth = async () => {
     try {
-      const newUser = await signInWithGoogle()
-      if(!newUser && error){
-        showToast("Error", error.message, "error")
-        return
+      const newUser = await signInWithGoogle();
+      if (!newUser && error) {
+        showToast("Error", error.message, "error");
+        return;
       }
-      if(newUser) {
+      const userRef = doc(firestore, "users", newUser.user.uid);
+      const userSnap = await getDoc(userRef);
+
+      if (userSnap.exists()) {
+        const userDoc = userSnap.data();
+        localStorage.setItem("user-info", JSON.stringify(userDoc));
+        loginUser(userDoc);
+      } else {
         const userDoc = {
           uid: newUser.user.uid,
           email: newUser.user.email,
@@ -27,19 +34,24 @@ const GoogleAuth = ({prefix}) => {
           lastName: newUser.user.displayName.split(" ")[1],
           savedArticles: [],
           savedBooks: [],
-          signedUpAt:Date.now()
+          signedUpAt: Date.now(),
         };
-        await setDoc(doc(firestore, "users", newUser.user.uid), userDoc)
-        localStorage.setItem("user-info", JSON.stringify(userDoc))
-        loginUser(userDoc)
+        await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
+        localStorage.setItem("user-info", JSON.stringify(userDoc));
+        loginUser(userDoc);
       }
     } catch (error) {
-      showToast("Error", error.message, "error")
+      showToast("Error", error.message, "error");
     }
-  }
+  };
   return (
     <>
-      <Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"} onClick={handleGoogleAuth}>
+      <Flex
+        alignItems={"center"}
+        justifyContent={"center"}
+        cursor={"pointer"}
+        onClick={handleGoogleAuth}
+      >
         <FcGoogle fontSize={25} />
         <Text mx={2}>{prefix} with Google</Text>
       </Flex>
